877a227bbb88e3253695eeaa04183eb45f65484b
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2023-02-02 20:24:36 -0300

    update my description

--- sections/About.jsx
+++ sections/About.jsx
@@ -22,10 +22,12 @@ const About = () => (
         variants={fadeIn('up', 'tween', 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        <span className="font-extrabold text-white">Seja Bem-Vindo</span>  
-         <br></br>Este daqui é um pouquinho de mim, eu vivo no planeta do {' '}
+        <span className="font-extrabold text-white">Metaverse</span> is a new
+        thing in the future, where you can enjoy the virtual world by feeling
+        like it's really real, you can feel what you feel in this metaverse
+        world, because this is really the{' '}
         <span className="font-extrabold text-white">
-        construindo o futuro
+          madness of the metaverse
         </span>{' '}
         of today, using only{' '}
         <span className="font-extrabold text-white">VR</span> devices you can
